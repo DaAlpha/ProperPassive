0b3a76c3fe558d8f67bf5642490ce66281b9ea50
DaAlpha <daalpha@nologam.com>
2016-01-03 14:33:30 +0100

    Fixed/ajusted player tags

--- client/cPassive.lua
+++ client/cPassive.lua
@@ -3,10 +3,11 @@ class 'Passive'
 
 function Passive:__init()
 	-- Settings
-	self.maxDistance	= 200			-- Maximum distance for the "Passive" tags in meters
-	self.passiveText	= "Passive"		-- Text for the "Passive" tags
-	self.textSize		= 14			-- Text size for the "Passive" tags
 	self.cooldown		= 60			-- seconds (Default: 60)
+	self.tagOffset		= 20			-- px (Default: 20)
+	self.textSize		= 14			-- pt (Default: 14) Text size for the tags
+	self.passiveText	= "Passive"		-- Text for the "Passive" tags
+	self.passiveColor	= Color(0, 222, 0)	-- Color used for tags and info text
 
 	-- Globals
 	self.timer		= Timer()
@@ -70,22 +71,20 @@ function Passive:Render()
 
 	for player in Client:GetStreamedPlayers() do
 		if player:GetValue("Passive") then
-			local tagpos	= player:GetBonePosition("ragdoll_Head") + Vector3(0, 0.5, 0)
+			local tagpos	= player:GetBonePosition("ragdoll_Head") + Vector3(0, 0.3, 0)
 			local distance	= tagpos:Distance(LocalPlayer:GetPosition())
+			local pos, onsc	= Render:WorldToScreen(tagpos)
 
-			if distance < self.maxDistance then
-				local pos, onscreen = Render:WorldToScreen(tagpos)
+			if onsc then
+				local scale		= math.clamp(1 - distance / 1000, 0.75, 1)
+				local size		= Render:GetTextSize(self.passiveText, self.textSize, scale)
+				pos				= pos - Vector2(size.x / 2, size.y + self.tagOffset * scale)
+				local sColor	= Color(0, 0, 0, 180 * scale ^ 2)
+				local color		= Copy(self.passiveColor)
+				color.a			= 255 * scale
 
-				if onscreen then
-					local factor	= math.clamp(1 - distance / self.maxDistance, 0, 1)
-					local width		= Render:GetTextWidth(self.passiveText, self.textSize)
-					pos				= pos - Vector2(width / 2, math.clamp(300 * factor, 0, 26))
-					local color		= Color(0, 255, 0, 255 * factor)
-					local sColor	= Color(0, 0, 0, 200 * factor)
-
-					Render:DrawText(pos + Vector2.One, self.passiveText, sColor, self.textSize)
-					Render:DrawText(pos, self.passiveText, color, self.textSize)
-				end
+				Render:DrawText(pos + Vector2.One, self.passiveText, sColor, self.textSize, scale)
+				Render:DrawText(pos, self.passiveText, color, self.textSize, scale)
 			end
 		end
 	end
@@ -95,23 +94,22 @@ function Passive:Render()
 		local textpos = Vector2(Render.Width/2 - width/2, 5)
 
 		Render:DrawText(textpos + Vector2.One, "Passive", Color.Black, 18)
-		Render:DrawText(textpos, "Passive", Color.Lime, 18)
+		Render:DrawText(textpos, "Passive", self.passiveColor, 18)
 	end
 end
 
 function Passive:ModuleLoad()
 	Events:Fire("HelpAddItem", {
 		name = "Passive Mode",
-		text = "Type /passive into the chat to the chat to toggle passive mode.\n" ..
-				"\n" ..
-				"If you are in passive mode, you cannot fire your weapons and you cannot kill others " ..
-				"(except from roadkilling, that is pretty much impossible to code properly) " ..
-				"and you are completely invincible. If a vehicle's driver is passive, " ..
-				"the vehicle will become invulnerable until he exits the vehicle. " ..
-				"If a vehicle is invulnerable, its mounted guns cannot be used, " ..
-				"even if the person using it is not passive.\n" ..
-				"\n" ..
-				"Passive Mode (public version) by DaAlpha, coder of Nologam"})
+		text = 
+			"Type /passive into the chat to the chat to toggle passive mode.\n \n" ..
+			"If you are in passive mode, you cannot fire your weapons and you cannot kill others " ..
+			"and you are completely invincible. If a vehicle's driver is passive, " ..
+			"the vehicle will become invulnerable until he exits the vehicle. " ..
+			"If a vehicle is invulnerable, its mounted guns cannot be used, " ..
+			"even if the person using it is not passive.\n \n" ..
+			"Passive Mode (public version) by DaAlpha, coder of Nologam"
+			})
 end
 
 function Passive:ModuleUnload()
