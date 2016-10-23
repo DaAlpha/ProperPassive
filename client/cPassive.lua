00c022047d40b86d2bf0ba505bd18c85d305d31f
DaAlpha <daalpha@nologam.com>
2016-10-23 13:28:30 +0200

    Changed indentations

--- client/cPassive.lua
+++ client/cPassive.lua
@@ -2,118 +2,118 @@
 class 'Passive'
 
 function Passive:__init()
-	-- Settings
-	self.cooldown		= 60			-- seconds (Default: 60)
-	self.tagOffset		= 20			-- px (Default: 20)
-	self.textSize		= 14			-- pt (Default: 14) Text size for the tags
-	self.passiveText	= "Passive"		-- Text for the "Passive" tags
-	self.passiveColor	= Color(0, 222, 0)	-- Color used for tags and info text
+  -- Settings
+  self.cooldown     = 60  -- seconds (Default: 60)
+  self.tagOffset    = 20  -- px (Default: 20)
+  self.textSize     = 14  -- pt (Default: 14) Text size for the tags
+  self.passiveText  = "Passive" -- Text for the "Passive" tags
+  self.passiveColor = Color(0, 222, 0) -- Color used for tags and info text
 
-	-- Globals
-	self.timer		= Timer()
-	self.actions	= {
-		[11] = true, [12] = true, [13] = true, [14] = true,
-		[15] = true, [137] = true, [138] = true, [139] = true
-		}
+  -- Globals
+  self.timer    = Timer()
+  self.actions  = {
+    [11] = true, [12] = true, [13] = true, [14] = true,
+    [15] = true, [137] = true, [138] = true, [139] = true
+    }
 
-	-- Events
-	Events:Subscribe("LocalPlayerChat", self, self.LocalPlayerChat)
-	Events:Subscribe("LocalPlayerInput", self, self.LocalPlayerInput)
-	Events:Subscribe("LocalPlayerBulletHit", self, self.LocalPlayerDamage)
-	Events:Subscribe("LocalPlayerExplosionHit", self, self.LocalPlayerDamage)
-	Events:Subscribe("LocalPlayerForcePulseHit", self, self.LocalPlayerDamage)
-	Events:Subscribe("NetworkObjectValueChange", self, self.NetworkObjectValueChange)
-	Events:Subscribe("Render", self, self.Render)
-    Events:Subscribe("ModuleLoad", self, self.ModuleLoad)
-    Events:Subscribe("ModuleUnload", self, self.ModuleUnload)
+  -- Events
+  Events:Subscribe("LocalPlayerChat", self, self.LocalPlayerChat)
+  Events:Subscribe("LocalPlayerInput", self, self.LocalPlayerInput)
+  Events:Subscribe("LocalPlayerBulletHit", self, self.LocalPlayerDamage)
+  Events:Subscribe("LocalPlayerExplosionHit", self, self.LocalPlayerDamage)
+  Events:Subscribe("LocalPlayerForcePulseHit", self, self.LocalPlayerDamage)
+  Events:Subscribe("NetworkObjectValueChange", self, self.NetworkObjectValueChange)
+  Events:Subscribe("Render", self, self.Render)
+  Events:Subscribe("ModuleLoad", self, self.ModuleLoad)
+  Events:Subscribe("ModuleUnload", self, self.ModuleUnload)
 end
 
 function Passive:LocalPlayerChat(args)
-	if args.text:lower() ~= "/passive" then return end
+  if args.text:lower() ~= "/passive" then return end
 
-	local seconds = self.timer:GetSeconds()
-	if seconds < self.cooldown then
-		Chat:Print("Cooling down. " .. math.ceil(self.cooldown - seconds) .. " seconds remaining.", Color.Red)
-		return false
-	end
+  local seconds = self.timer:GetSeconds()
+  if seconds < self.cooldown then
+    Chat:Print("Cooling down. " .. math.ceil(self.cooldown - seconds) .. " seconds remaining.", Color.Red)
+    return false
+  end
 
-	Network:Send("Toggle", not LocalPlayer:GetValue("Passive"))
-	self.timer:Restart()
-	return false
+  Network:Send("Toggle", not LocalPlayer:GetValue("Passive"))
+  self.timer:Restart()
+  return false
 end
 
 function Passive:LocalPlayerInput(args)
-	if self.actions[args.input] and (LocalPlayer:GetValue("Passive")
-			or LocalPlayer:InVehicle() and LocalPlayer:GetVehicle():GetInvulnerable()) then
-		return false
-	end
+  if self.actions[args.input] and (LocalPlayer:GetValue("Passive")
+      or LocalPlayer:InVehicle() and LocalPlayer:GetVehicle():GetInvulnerable()) then
+    return false
+  end
 end
 
 function Passive:LocalPlayerDamage(args)
-	if LocalPlayer:GetValue("Passive") or args.attacker and (args.attacker:GetValue("Passive")
-			or args.attacker:InVehicle() and args.attacker:GetVehicle():GetInvulnerable()) then
-		return false
-	end
+  if LocalPlayer:GetValue("Passive") or args.attacker and (args.attacker:GetValue("Passive")
+      or args.attacker:InVehicle() and args.attacker:GetVehicle():GetInvulnerable()) then
+    return false
+  end
 end
 
 function Passive:NetworkObjectValueChange(args)
-	if args.key == "Passive" and args.object.__type == "LocalPlayer" then
-		if args.value then
-			Game:FireEvent("ply.invulnerable")
-		else
-			Game:FireEvent("ply.vulnerable")
-		end
-	end
+  if args.key == "Passive" and args.object.__type == "LocalPlayer" then
+    if args.value then
+      Game:FireEvent("ply.invulnerable")
+    else
+      Game:FireEvent("ply.vulnerable")
+    end
+  end
 end
 
 function Passive:Render()
-	if Game:GetState() ~= GUIState.Game then return end
+  if Game:GetState() ~= GUIState.Game then return end
 
-	for player in Client:GetStreamedPlayers() do
-		if player:GetValue("Passive") then
-			local tagpos	= player:GetBonePosition("ragdoll_Head") + Vector3(0, 0.3, 0)
-			local distance	= tagpos:Distance(LocalPlayer:GetPosition())
-			local pos, onsc	= Render:WorldToScreen(tagpos)
+  for player in Client:GetStreamedPlayers() do
+    if player:GetValue("Passive") then
+      local tagpos    = player:GetBonePosition("ragdoll_Head") + Vector3(0, 0.3, 0)
+      local distance  = tagpos:Distance(LocalPlayer:GetPosition())
+      local pos, onsc = Render:WorldToScreen(tagpos)
 
-			if onsc then
-				local scale		= math.clamp(1 - distance / 1000, 0.75, 1)
-				local size		= Render:GetTextSize(self.passiveText, self.textSize, scale)
-				pos				= pos - Vector2(size.x / 2, size.y + self.tagOffset * scale)
-				local sColor	= Color(0, 0, 0, 180 * scale ^ 2)
-				local color		= Copy(self.passiveColor)
-				color.a			= 255 * scale
+      if onsc then
+        local scale   = math.clamp(1 - distance / 1000, 0.75, 1)
+        local size    = Render:GetTextSize(self.passiveText, self.textSize, scale)
+        pos           = pos - Vector2(size.x / 2, size.y + self.tagOffset * scale)
+        local sColor  = Color(0, 0, 0, 180 * scale ^ 2)
+        local color   = Copy(self.passiveColor)
+        color.a       = 255 * scale
 
-				Render:DrawText(pos + Vector2.One, self.passiveText, sColor, self.textSize, scale)
-				Render:DrawText(pos, self.passiveText, color, self.textSize, scale)
-			end
-		end
-	end
+        Render:DrawText(pos + Vector2.One, self.passiveText, sColor, self.textSize, scale)
+        Render:DrawText(pos, self.passiveText, color, self.textSize, scale)
+      end
+    end
+  end
 
-	if LocalPlayer:GetValue("Passive") then
-		local width = Render:GetTextWidth("Passive")
-		local textpos = Vector2(Render.Width/2 - width/2, 5)
+  if LocalPlayer:GetValue("Passive") then
+    local width = Render:GetTextWidth("Passive")
+    local textpos = Vector2(Render.Width/2 - width/2, 5)
 
-		Render:DrawText(textpos + Vector2.One, "Passive", Color.Black, 18)
-		Render:DrawText(textpos, "Passive", self.passiveColor, 18)
-	end
+    Render:DrawText(textpos + Vector2.One, "Passive", Color.Black, 18)
+    Render:DrawText(textpos, "Passive", self.passiveColor, 18)
+  end
 end
 
 function Passive:ModuleLoad()
-	Events:Fire("HelpAddItem", {
-		name = "Passive Mode",
-		text = 
-			"Type /passive into the chat to the chat to toggle passive mode.\n \n" ..
-			"If you are in passive mode, you cannot fire your weapons and you cannot kill others " ..
-			"and you are completely invincible. If a vehicle's driver is passive, " ..
-			"the vehicle will become invulnerable until he exits the vehicle. " ..
-			"If a vehicle is invulnerable, its mounted guns cannot be used, " ..
-			"even if the person using it is not passive.\n \n" ..
-			"Passive Mode (public version) by DaAlpha, coder of Nologam"
-			})
+  Events:Fire("HelpAddItem", {
+    name = "Passive Mode",
+    text =
+      "Type /passive into the chat to the chat to toggle passive mode.\n \n" ..
+      "If you are in passive mode, you cannot fire your weapons and you cannot kill others " ..
+      "and you are completely invincible. If a vehicle's driver is passive, " ..
+      "the vehicle will become invulnerable until he exits the vehicle. " ..
+      "If a vehicle is invulnerable, its mounted guns cannot be used, " ..
+      "even if the person using it is not passive.\n \n" ..
+      "Passive Mode (public version) by DaAlpha, coder of Nologam"
+      })
 end
 
 function Passive:ModuleUnload()
-	Events:Fire("HelpRemoveItem", {name = "Passive Mode"})
+  Events:Fire("HelpRemoveItem", {name = "Passive Mode"})
 end
 
 local passive = Passive()
--- server/sPassive.lua
+++ server/sPassive.lua
@@ -2,84 +2,84 @@
 class 'Passive'
 
 function Passive:__init()
-	-- Settings
-	self.remTime	= 7		-- Days
-	self.interval	= 1		-- Hours
+  -- Settings
+  self.remTime  = 7 -- Days
+  self.interval = 1 -- Hours
 
-	-- Globals
-	self.passives	= {}
-	self.timer		= Timer()
+  -- Globals
+  self.passives = {}
+  self.timer    = Timer()
 
-	-- Create DB table if it does not exist
-	SQL:Execute("CREATE TABLE IF NOT EXISTS passive (steamid VARCHAR PRIMARY KEY)")
+  -- Create DB table if it does not exist
+  SQL:Execute("CREATE TABLE IF NOT EXISTS passive (steamid VARCHAR PRIMARY KEY)")
 
-	-- Load SQL entries to the cache initialls
-	local timestamp = os.time()
-	for _, entry in ipairs(SQL:Query("SELECT * FROM passive"):Execute()) do
-		self.passives[entry.steamid] = timestamp
-	end
+  -- Load SQL entries to the cache initialls
+  local timestamp = os.time()
+  for _, entry in ipairs(SQL:Query("SELECT * FROM passive"):Execute()) do
+    self.passives[entry.steamid] = timestamp
+  end
 
-	-- Network
-	Network:Subscribe("Toggle", self, self.Toggle)
+  -- Network
+  Network:Subscribe("Toggle", self, self.Toggle)
 
-	-- Events
-	Events:Subscribe("ClientModuleLoad", self, self.ClientModuleLoad)
-	Events:Subscribe("PlayerEnterVehicle", self, self.PlayerEnterVehicle)
-	Events:Subscribe("PostTick", self, self.PostTick)
-	Events:Subscribe("ModuleUnload", self, self.ModuleUnload)
+  -- Events
+  Events:Subscribe("ClientModuleLoad", self, self.ClientModuleLoad)
+  Events:Subscribe("PlayerEnterVehicle", self, self.PlayerEnterVehicle)
+  Events:Subscribe("PostTick", self, self.PostTick)
+  Events:Subscribe("ModuleUnload", self, self.ModuleUnload)
 end
 
 function Passive:Toggle(state, sender)
-	sender:SetNetworkValue("Passive", state or nil)
+  sender:SetNetworkValue("Passive", state or nil)
 
-	local vehicle = sender:GetVehicle()
-	if IsValid(vehicle) and vehicle:GetDriver() == sender then
-		vehicle:SetInvulnerable(state)
-	end
+  local vehicle = sender:GetVehicle()
+  if IsValid(vehicle) and vehicle:GetDriver() == sender then
+    vehicle:SetInvulnerable(state)
+  end
 
-	Chat:Send(sender, "Passive mode " .. (state and "enabled." or "disabled."), Color.Lime)
+  Chat:Send(sender, "Passive mode " .. (state and "enabled." or "disabled."), Color.Lime)
 
-	self.passives[sender:GetSteamId().string] = state and os.time() or nil
+  self.passives[sender:GetSteamId().string] = state and os.time() or nil
 end
 
 function Passive:ClientModuleLoad(args)
-	local steamid = args.player:GetSteamId().string
-	local state = self.passives[steamid]
-	args.player:SetNetworkValue("Passive", state and true or false)
-	self.passives[steamid] = state and os.time() or nil
+  local steamid = args.player:GetSteamId().string
+  local state = self.passives[steamid]
+  args.player:SetNetworkValue("Passive", state and true or false)
+  self.passives[steamid] = state and os.time() or nil
 
-	local vehicle = args.player:GetVehicle()
-	if IsValid(vehicle) and vehicle:GetDriver() == args.player then
-		vehicle:SetInvulnerable(state ~= nil)
-	end
+  local vehicle = args.player:GetVehicle()
+  if IsValid(vehicle) and vehicle:GetDriver() == args.player then
+    vehicle:SetInvulnerable(state ~= nil)
+  end
 end
 
 function Passive:PlayerEnterVehicle(args)
-	if args.is_driver then args.vehicle:SetInvulnerable(args.player:GetValue("Passive") == true) end
+  if args.is_driver then args.vehicle:SetInvulnerable(args.player:GetValue("Passive") == true) end
 end
 
 function Passive:PostTick()
-	if self.timer:GetHours() > self.interval then
-		local threshold = os.time() - self.remTime * 86400
-		for steamid, timestamp in pairs(self.passives) do
-			if timestamp < threshold then
-				self.passives[steamid] = nil
-			end
-		end
-		self:ModuleUnload()
-		self.timer:Restart()
-	end
+  if self.timer:GetHours() > self.interval then
+    local threshold = os.time() - self.remTime * 86400
+    for steamid, timestamp in pairs(self.passives) do
+      if timestamp < threshold then
+        self.passives[steamid] = nil
+      end
+    end
+    self:ModuleUnload()
+    self.timer:Restart()
+  end
 end
 
 function Passive:ModuleUnload()
-	local trans = SQL:Transaction()
-	SQL:Execute("DELETE FROM passive")
-	for steamid, _ in pairs(self.passives) do
-		local command = SQL:Command("INSERT INTO passive VALUES (?)")
-		command:Bind(1, steamid)
-		command:Execute()
-	end
-	trans:Commit()
+  local trans = SQL:Transaction()
+  SQL:Execute("DELETE FROM passive")
+  for steamid, _ in pairs(self.passives) do
+    local command = SQL:Command("INSERT INTO passive VALUES (?)")
+    command:Bind(1, steamid)
+    command:Execute()
+  end
+  trans:Commit()
 end
 
 local passive = Passive()
